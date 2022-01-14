325bf6ee462cc65a5177e92805ab34b5b0b45479
Carmen <pr.carmengalvan@gmail.com>
2022-01-14 11:57:22 +0100

    Control check laboratorio simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,13 +3,24 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface ProductRepository {
-    List<Product> findAll();
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+	@Query("SELECT p FROM ProductType p")
     List<ProductType> findAllProductTypes();
+	
+	@Query("SELECT p FROM ProductType p WHERE p.name=:n")
+	ProductType findProductType(@Param("n")String name);
+	
+	@Query("SELECT p FROM Product p WHERE p.price<:umbral")
+	List<Product> findByPriceLessThan(@Param("umbral")Double umbral);
+	
+    List<Product> findAll();
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
